f2f2949ea5c820328cc9588debca2f75999fa8a6
Kieran James Challis <rc0033607@student.riversidecollege.ac.uk>
2018-12-04 10:49:18 +0000

    added functions

--- FORCES/Form1.cs
+++ FORCES/Form1.cs
@@ -16,5 +16,39 @@ namespace FORCES
         {
             InitializeComponent();
         }
+
+        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+            
+        }
+        double sin(double a)
+        {
+            double rad = a / 180.0 * Math.PI;
+            return Math.Sin(rad);
+        }
+        double cos(double a)
+        {
+            double rad = a / 180.0 * Math.PI;
+            return Math.Cos(rad);
+        }
+        double tan(double a)
+        {
+            double rad = a / 180.0 * Math.PI;
+            return Math.Tan(rad);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double Angle = double.Parse(textBox1.Text);
+                double ans = sin(Angle);
+                label1.Text = ans.ToString();
+            }
+            catch
+            {
+                MessageBox.Show("enter a number ");
+            }
+        }
     }
 }
